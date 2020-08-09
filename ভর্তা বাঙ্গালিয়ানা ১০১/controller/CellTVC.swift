b0677c946a724a66a8bdca0bc0eaebb2e5096438
Asif Mimi <30404856+asifmimi1@users.noreply.github.com>
2020-08-10 02:02:49 +0600

    Vorta Bangaliyana 1st day: 1. Home VC done

---
+++ ভর্তা বাঙ্গালিয়ানা ১০১/controller/CellTVC.swift
@@ -0,0 +1,26 @@
+//
+//  CellTVC.swift
+//  ভর্তা বাঙ্গালিয়ানা ১০১
+//
+//  Created by Asif's Mac on 10/8/20.
+//  Copyright © 2020 Asif's Mac. All rights reserved.
+//
+
+import UIKit
+
+class CellTVC: UITableViewCell {
+
+    @IBOutlet weak var vortaImage: UIImageView!
+    @IBOutlet weak var vortaNamelbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ভর্তা বাঙ্গালিয়ানা ১০১/controller/HomeVC.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ভর্তা বাঙ্গালিয়ানা ১০১
+//
+//  Created by Asif's Mac on 9/8/20.
+//  Copyright © 2020 Asif's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let vortaItemArray = ["কাঁচামরিচ ভর্তা", "বেগুন ভর্তা", "বেগুন পুড়ে ভর্তা", "শুকনা মরিচ ভর্তা", "চেপা শুটকি ভর্তা"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// MARK: - Table View
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    // MARK:- Table view datasource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vortaItemArray.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CellTVC
+        cell?.vortaNamelbl.text = vortaItemArray[indexPath.row]
+        cell?.vortaImage.image = UIImage(named: vortaItemArray[indexPath.row])
+        return cell!
+    }
+    
+    
+}
+
